2e0ab87c558c139cb7e922b9c1d78cc013e79afc
imkks <imkrsh007@gmail.com>
2022-11-09 20:25:04 +0530

    apply loan done

--- src/components/ApplyLoan.js
+++ src/components/ApplyLoan.js
@@ -1,15 +1,56 @@
+import { useRef } from "react"
+import { useNavigate } from "react-router-dom";
+
 const ApplyLoan = () => {
+    const formEl=useRef(null);
+    const naviate=useNavigate()
+    const submitHandler=async e=>{
+        e.preventDefault();
+       let result= await applyLoan(formEl.current[0].value,formEl.current[1].value,formEl.current[2].value,formEl.current[3].value)
+       if(result)
+            naviate("/")
+
+    }
+    const applyLoan=async (userId,desc,make,cat,value)=>{
+        let myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+let raw = JSON.stringify({
+  "employeeId": userId,
+  "itemDescription": desc,
+  "issueStatus": "N",
+  "itemMake": make,
+  "itemCategory": cat,
+  "itemValuation": value
+});
+
+let requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+let response=await fetch("localhost:8085/luma/loan_apply", requestOptions)
+let result=response.json();
+return result;
+    }
+
     return (
         <div>
             <h2>Apply for Loan</h2>
-            <form>
+            <form ref={formEl} onSubmit={submitHandler}>
                 <div className="col">
                 <div className="form-block">
-                <label>Loan Amount</label>
+                <label>Employee Id</label>
                 <input type="text"></input>
                 </div>
                 <div className="form-block">
                 <label>Item Description</label>
+                <input type="text"></input>
+                </div>
+                <div className="form-block">
+                <label>Item Category</label>
                 <input type="text"></input>
                 </div>
                 </div>
@@ -17,6 +58,10 @@ const ApplyLoan = () => {
                 <div className="form-block">
                 <label>Item Value</label>
                 <input type="number"></input>
+                </div>
+                <div className="form-block">
+                <label>Item Make</label>
+                <input type="text"></input>
                 </div>
                 </div>
                 <button className="button" type="submit">Apply Loan</button>
